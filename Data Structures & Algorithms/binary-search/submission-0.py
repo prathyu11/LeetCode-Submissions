cd55628ae7cd1cd12c4248dfca99c71f5bf17af7
Prathyusha K <62874096+prathyu11@users.noreply.github.com>
2026-08-17 18:37:17 -0500

    Add: binary-search - submission-0

---
+++ Data Structures & Algorithms/binary-search/submission-0.py
@@ -0,0 +1,18 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        start = 0
+        end = len(nums)-1
+        
+
+        while start<=end:
+            mid = start+(end-start)//2 #same as (l+r)//2 but never computes a large intermediate sum.
+            if target == nums[mid]:
+                return mid
+            elif target > nums[mid]:
+                start = mid+1
+            else:
+                end = mid-1
+                
+        return -1
+        
+
